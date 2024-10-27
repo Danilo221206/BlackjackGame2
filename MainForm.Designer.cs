2697e6df52ce044ca66abb69c9318939f00034ca
Danilo221206 <danilo221206@gmail.com>
2024-10-27 11:31:59 -0300

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace BlackjackGame2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbldinheiro = new Label();
+            Playerscore = new Label();
+            Dealerscore = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            cartaplayer1 = new PictureBox();
+            cartaplayer2 = new PictureBox();
+            cartaplayer3 = new PictureBox();
+            cartaplayer4 = new PictureBox();
+            cartadealer1 = new PictureBox();
+            cartadealer2 = new PictureBox();
+            cartadealer3 = new PictureBox();
+            cartadealer4 = new PictureBox();
+            cartaplayer5 = new PictureBox();
+            Hitbtn = new Button();
+            Doublebtn = new Button();
+            Standbtn = new Button();
+            Betbtn = new Button();
+            Aposta = new NumericUpDown();
+            richTextBox1 = new RichTextBox();
+            Dinheiro = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)cartaplayer1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cartaplayer2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cartaplayer3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cartaplayer4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cartadealer1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cartadealer2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cartadealer3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cartadealer4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cartaplayer5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Aposta).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Dinheiro).BeginInit();
+            SuspendLayout();
+            // 
+            // lbldinheiro
+            // 
+            lbldinheiro.AutoSize = true;
+            lbldinheiro.Location = new Point(22, 21);
+            lbldinheiro.Name = "lbldinheiro";
+            lbldinheiro.Size = new Size(76, 15);
+            lbldinheiro.TabIndex = 0;
+            lbldinheiro.Text = "Seu dinheiro:";
+            // 
+            // Playerscore
+            // 
+            Playerscore.AutoSize = true;
+            Playerscore.Location = new Point(151, 9);
+            Playerscore.Name = "Playerscore";
+            Playerscore.Size = new Size(89, 15);
+            Playerscore.TabIndex = 1;
+            Playerscore.Text = "Sua pontuação:";
+            // 
+            // Dealerscore
+            // 
+            Dealerscore.AutoSize = true;
+            Dealerscore.Location = new Point(710, 9);
+            Dealerscore.Name = "Dealerscore";
+            Dealerscore.Size = new Size(119, 15);
+            Dealerscore.TabIndex = 2;
+            Dealerscore.Text = "Pontuação do dealer:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(299, 476);
+            label4.Name = "label4";
+            label4.Size = new Size(65, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Suas cartas";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(820, 476);
+            label5.Name = "label5";
+            label5.Size = new Size(93, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Cartas do Dealer";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(22, 249);
+            label6.Name = "label6";
+            label6.Size = new Size(67, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Sua aposta:";
+            // 
+            // cartaplayer1
+            // 
+            cartaplayer1.Location = new Point(142, 39);
+            cartaplayer1.Name = "cartaplayer1";
+            cartaplayer1.Size = new Size(317, 434);
+            cartaplayer1.TabIndex = 6;
+            cartaplayer1.TabStop = false;
+            // 
+            // cartaplayer2
+            // 
+            cartaplayer2.Location = new Point(201, 39);
+            cartaplayer2.Name = "cartaplayer2";
+            cartaplayer2.Size = new Size(317, 434);
+            cartaplayer2.TabIndex = 7;
+            cartaplayer2.TabStop = false;
+            // 
+            // cartaplayer3
+            // 
+            cartaplayer3.Location = new Point(270, 39);
+            cartaplayer3.Name = "cartaplayer3";
+            cartaplayer3.Size = new Size(317, 434);
+            cartaplayer3.TabIndex = 8;
+            cartaplayer3.TabStop = false;
+            cartaplayer3.Visible = false;
+            // 
+            // cartaplayer4
+            // 
+            cartaplayer4.Location = new Point(323, 39);
+            cartaplayer4.Name = "cartaplayer4";
+            cartaplayer4.Size = new Size(317, 434);
+            cartaplayer4.TabIndex = 9;
+            cartaplayer4.TabStop = false;
+            // 
+            // cartadealer1
+            // 
+            cartadealer1.Location = new Point(696, 39);
+            cartadealer1.Name = "cartadealer1";
+            cartadealer1.Size = new Size(317, 434);
+            cartadealer1.TabIndex = 10;
+            cartadealer1.TabStop = false;
+            // 
+            // cartadealer2
+            // 
+            cartadealer2.Location = new Point(752, 39);
+            cartadealer2.Name = "cartadealer2";
+            cartadealer2.Size = new Size(317, 434);
+            cartadealer2.TabIndex = 11;
+            cartadealer2.TabStop = false;
+            // 
+            // cartadealer3
+            // 
+            cartadealer3.Location = new Point(820, 39);
+            cartadealer3.Name = "cartadealer3";
+            cartadealer3.Size = new Size(317, 434);
+            cartadealer3.TabIndex = 12;
+            cartadealer3.TabStop = false;
+            // 
+            // cartadealer4
+            // 
+            cartadealer4.Location = new Point(894, 39);
+            cartadealer4.Name = "cartadealer4";
+            cartadealer4.Size = new Size(317, 434);
+            cartadealer4.TabIndex = 13;
+            cartadealer4.TabStop = false;
+            // 
+            // cartaplayer5
+            // 
+            cartaplayer5.Location = new Point(382, 39);
+            cartaplayer5.Name = "cartaplayer5";
+            cartaplayer5.Size = new Size(317, 434);
+            cartaplayer5.TabIndex = 14;
+            cartaplayer5.TabStop = false;
+            cartaplayer5.Visible = false;
+            // 
+            // Hitbtn
+            // 
+            Hitbtn.Location = new Point(24, 108);
+            Hitbtn.Name = "Hitbtn";
+            Hitbtn.Size = new Size(75, 23);
+            Hitbtn.TabIndex = 15;
+            Hitbtn.Text = "Hit";
+            Hitbtn.UseVisualStyleBackColor = true;
+            Hitbtn.Click += Hitbtn_Click;
+            // 
+            // Doublebtn
+            // 
+            Doublebtn.Location = new Point(24, 137);
+            Doublebtn.Name = "Doublebtn";
+            Doublebtn.Size = new Size(75, 23);
+            Doublebtn.TabIndex = 16;
+            Doublebtn.Text = "Double";
+            Doublebtn.UseVisualStyleBackColor = true;
+            // 
+            // Standbtn
+            // 
+            Standbtn.Location = new Point(24, 166);
+            Standbtn.Name = "Standbtn";
+            Standbtn.Size = new Size(75, 23);
+            Standbtn.TabIndex = 17;
+            Standbtn.Text = "Stand";
+            Standbtn.UseVisualStyleBackColor = true;
+            Standbtn.Click += Standbtn_Click;
+            // 
+            // Betbtn
+            // 
+            Betbtn.Location = new Point(22, 223);
+            Betbtn.Name = "Betbtn";
+            Betbtn.Size = new Size(75, 23);
+            Betbtn.TabIndex = 18;
+            Betbtn.Text = "Bet";
+            Betbtn.UseVisualStyleBackColor = true;
+            Betbtn.Click += button4_Click;
+            // 
+            // Aposta
+            // 
+            Aposta.Location = new Point(24, 267);
+            Aposta.Maximum = new decimal(new int[] { 1410065408, 2, 0, 0 });
+            Aposta.Name = "Aposta";
+            Aposta.Size = new Size(75, 23);
+            Aposta.TabIndex = 19;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(22, 305);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(114, 186);
+            richTextBox1.TabIndex = 20;
+            richTextBox1.Text = "";
+            // 
+            // Dinheiro
+            // 
+            Dinheiro.Location = new Point(24, 39);
+            Dinheiro.Maximum = new decimal(new int[] { 1410065408, 2, 0, 0 });
+            Dinheiro.Name = "Dinheiro";
+            Dinheiro.ReadOnly = true;
+            Dinheiro.Size = new Size(75, 23);
+            Dinheiro.TabIndex = 21;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1254, 528);
+            Controls.Add(Dinheiro);
+            Controls.Add(richTextBox1);
+            Controls.Add(Aposta);
+            Controls.Add(Betbtn);
+            Controls.Add(Standbtn);
+            Controls.Add(Doublebtn);
+            Controls.Add(Hitbtn);
+            Controls.Add(cartaplayer5);
+            Controls.Add(cartadealer4);
+            Controls.Add(cartadealer3);
+            Controls.Add(cartadealer2);
+            Controls.Add(cartadealer1);
+            Controls.Add(cartaplayer4);
+            Controls.Add(cartaplayer3);
+            Controls.Add(cartaplayer2);
+            Controls.Add(cartaplayer1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(Dealerscore);
+            Controls.Add(Playerscore);
+            Controls.Add(lbldinheiro);
+            Name = "MainForm";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)cartaplayer1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cartaplayer2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cartaplayer3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cartaplayer4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cartadealer1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cartadealer2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cartadealer3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cartadealer4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cartaplayer5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Aposta).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Dinheiro).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbldinheiro;
+        private Label Playerscore;
+        private Label Dealerscore;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private PictureBox cartaplayer1;
+        private PictureBox cartaplayer2;
+        private PictureBox cartaplayer3;
+        private PictureBox cartaplayer4;
+        private PictureBox cartadealer1;
+        private PictureBox cartadealer2;
+        private PictureBox cartadealer3;
+        private PictureBox cartadealer4;
+        private PictureBox cartaplayer5;
+        private Button Hitbtn;
+        private Button Doublebtn;
+        private Button Standbtn;
+        private Button Betbtn;
+        private NumericUpDown Aposta;
+        private RichTextBox richTextBox1;
+        private NumericUpDown Dinheiro;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,350 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace BlackjackGame2
+{
+    public partial class MainForm : Form
+    {
+        // Definir variáveis do jogador, dealer e o valor da aposta
+        private Player player;
+        private Dealer dealer;
+        private static readonly HttpClient client = new HttpClient();
+        private string deckId;
+        private bool betdone = false;
+        int action = 0;
+        public MainForm()
+        {
+            InitializeComponent();
+            player = new Player { Balance = 1000 }; // Exemplo de saldo inicial
+            dealer = new Dealer();
+            Dinheiro.Value = 1000;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        // Inicia uma nova rodada
+        private async void StartNewGame()
+        {
+            Playerscore.Text = "Sua pontuação:";
+            Dealerscore.Text = "Pontuação do dealer:";
+
+            deckId = await CreateDeck();
+            cartadealer1.Visible = false; cartadealer2.Visible = false; cartadealer3.Visible = false; cartadealer4.Visible = true;
+            cartaplayer1.Visible = false; cartaplayer2.Visible = false; cartaplayer3.Visible = false; cartaplayer4.Visible = false; cartaplayer5.Visible = false;
+            if (betdone == false) { 
+                MessageBox.Show("Novo jogo! Faça suas apostas antes de começar.");
+                Aposta.Enabled = true;
+                Hitbtn.Enabled = false;
+                Standbtn.Enabled = false;
+                Doublebtn.Enabled = false;
+
+            }
+            else await DealInitialCards();
+        }
+
+        // Cria um novo baralho embaralhado
+        private async Task<string> CreateDeck()
+        {
+            string url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+            var response = await client.GetStringAsync(url);
+            dynamic deck = JsonConvert.DeserializeObject(response);
+            return deck.deck_id;
+        }
+
+        // Distribui as cartas iniciais (2 para o jogador e 2 para o dealer)
+        private async Task DealInitialCards()
+        {
+            Hitbtn.Enabled = true;
+            Standbtn.Enabled = true;
+            Doublebtn.Enabled = true;
+            action = 0;
+            player.PlayerHand.Cards.Clear();
+            dealer.DealerHand.Cards.Clear();
+            ScoreManager scoreManager = new ScoreManager();
+            player.PlayerHand.Cards.Add(await DrawCard()); cartaplayer1.Visible = true;
+            dealer.DealerHand.Cards.Add(await DrawCard()); cartadealer1.Visible = true;
+            player.PlayerHand.Cards.Add(await DrawCard()); cartaplayer2.Visible = true;
+            dealer.DealerHand.Cards.Add(await DrawCard()); cartadealer2.Visible = true;
+            cartaplayer1.Load(player.PlayerHand.Cards[0].ImageUrl);
+            cartaplayer2.Load(player.PlayerHand.Cards[1].ImageUrl);
+            cartadealer1.Load(dealer.DealerHand.Cards[0].ImageUrl);
+            //int dealerScore = scoreManager.CalculateHandValue(dealer.DealerHand.Cards);
+            Dealerscore.Text = $"Pontuação do dealer: ?";
+            cartadealer2.Load(dealer.DealerHand.Cards[1].ImageUrl);
+            int playerScore = scoreManager.CalculateHandValue(player.PlayerHand.Cards);
+            Playerscore.Text = $"Sua pontuação: {playerScore}";
+            int dealerScore = scoreManager.CalculateHandValue(dealer.DealerHand.Cards);
+            Dealerscore.Text = $"Pontuação do dealer: {dealerScore}";
+            if (playerScore == 21)
+            {
+                MessageBox.Show("BLACKJACK!! Você ganhou");
+                Dinheiro.Value += Aposta.Value * 3 / 2;
+                betdone = false;
+                Aposta.Value = 0;
+                StartNewGame();
+            }
+
+            // Dealer recebe a primeira carta (virada para cima)
+
+        }
+
+        // Saca uma carta do baralho da API
+        private async Task<Card> DrawCard()
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/{deckId}/draw/?count=1";
+            var response = await client.GetStringAsync(url);
+            dynamic draw = JsonConvert.DeserializeObject(response);
+
+            return new Card
+            {
+                Value = draw.cards[0].value,
+                Suit = draw.cards[0].suit,
+                ImageUrl = draw.cards[0].image
+            };
+        }
+
+        // Jogador escolhe 'Hit' (pedir mais uma carta)
+        private async void Hitbtn_Click(object sender, EventArgs e)
+        {
+            ScoreManager scoreManager = new ScoreManager();
+
+            // O jogador recebe uma nova carta e ela é adicionada à mão
+            player.PlayerHand.Cards.Add(await DrawCard());
+
+            // Atualiza a imagem da nova carta (supondo que seja cartaplayer3, por exemplo)
+            if (action == 0)
+            {
+                cartaplayer3.Visible = true;
+                cartaplayer3.Load(player.PlayerHand.Cards[2].ImageUrl);
+                action++;
+            }
+            else if (action == 1)
+            {
+                cartaplayer4.Visible = true;
+                cartaplayer4.Load(player.PlayerHand.Cards[3].ImageUrl);
+                action++;
+            }
+            else if (action == 2)
+            {
+                cartaplayer5.Visible = true;
+                cartaplayer5.Load(player.PlayerHand.Cards[4].ImageUrl);
+                action++;
+            }
+
+            // Calcula e exibe a nova pontuação do jogador
+            int playerScore = scoreManager.CalculateHandValue(player.PlayerHand.Cards);
+            Playerscore.Text = $"Sua pontuação: {playerScore}";
+
+            // Verifica se o jogador estourou (bust)
+            if (player.PlayerHand.IsBust())
+            {
+                MessageBox.Show("Você perdeu! A mão passou de 21.");
+                betdone = false;
+                Aposta.Value = 0;
+                StartNewGame();
+            }
+        }
+
+        // Jogador escolhe 'Stand' (ficar com as cartas)
+        // Jogador escolhe 'Stand' (ficar com as cartas)
+        private async void Standbtn_Click(object sender, EventArgs e)
+        {
+            await dealer.Play(DrawCard, this); // Passa o formulário como parâmetro
+            CheckWinner();
+        }
+
+
+        // Verifica o vencedor após o turno do dealer
+        private async void CheckWinner()
+        {
+            if (player.PlayerHand.GetTotalValue() > dealer.DealerHand.GetTotalValue())
+            {
+                MessageBox.Show("Você ganhou!");
+                Dinheiro.Value += Aposta.Value * 2; // Jogador ganha o dobro da aposta
+            }
+            else
+            {
+                MessageBox.Show("O dealer ganhou!");
+            }
+            betdone = false;
+            Aposta.Value = 0;
+            StartNewGame();
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Betbtn!! nome bugoukkkk
+            if (Aposta.Value > 0)
+            {
+                if (betdone == false)
+                {
+                    Aposta.Enabled = false;
+                    decimal valor = (Dinheiro.Value - Aposta.Value);
+                    if ((valor) < 0) MessageBox.Show("Você não possui dinheiro para isso");
+                    else
+                    {
+                        Dinheiro.Value -= Aposta.Value;
+                        StartNewGame();
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Sua aposta não pode ser alterada até o final da partida!");
+                }
+                betdone = true;
+            }
+            else MessageBox.Show("Insira um valor válido.");
+        }
+
+
+
+        // Representa uma carta
+        public class Card
+        {
+            public string Value { get; set; }
+            public string Suit { get; set; }
+            public string ImageUrl { get; set; }
+
+            public int GetCardValue()
+            {
+                if (Value == "ACE") return 11;
+                if (Value == "KING" || Value == "QUEEN" || Value == "JACK") return 10;
+                return int.TryParse(Value, out int result) ? result : 0;
+            }
+        }
+
+        // Representa uma mão (cartas de um jogador ou dealer)
+        public class Hand
+        {
+            public List<Card> Cards { get; set; } = new List<Card>();
+
+            public int GetTotalValue()
+            {
+                int totalValue = Cards.Sum(card => card.GetCardValue());
+                int aceCount = Cards.Count(card => card.Value == "ACE");
+
+                // Ajusta o valor do Ás de 11 para 1 se necessário
+                while (totalValue > 21 && aceCount > 0)
+                {
+                    totalValue -= 10;
+                    aceCount--;
+                }
+
+                return totalValue;
+            }
+
+            public bool IsBlackjack() => GetTotalValue() == 21 && Cards.Count == 2;
+            public bool IsBust() => GetTotalValue() > 21;
+        }
+
+        // Representa o Dealer
+        public class Dealer
+        {
+            public Hand DealerHand { get; set; } = new Hand();
+
+            // Dealer joga até atingir 17 ou mais
+            // Dealer joga até atingir 17 ou mais
+            public async Task Play(Func<Task<Card>> drawCardFunc, MainForm form)
+            {
+                while (DealerHand.GetTotalValue() < 17)
+                {
+                    // O dealer saca uma nova carta usando a função fornecida
+                    Card newCard = await drawCardFunc();
+                    DealerHand.Cards.Add(newCard);
+
+                    // Atualiza a interface gráfica
+                    if (!form.cartadealer3.Visible)
+                    {
+                        form.cartadealer3.Visible = true;
+                        form.cartadealer3.Load(newCard.ImageUrl);
+                    }
+                    else if (!form.cartadealer4.Visible)
+                    {
+                        form.cartadealer4.Visible = true;
+                        form.cartadealer4.Load(newCard.ImageUrl);
+                    }
+                }
+            }
+
+
+            // Simular carta do dealer (ajustar com a API)
+            private Card DrawCardFromDeck()
+            {
+                // Este método deve ser implementado para funcionar corretamente
+                return new Card(); // Exemplo
+            }
+        }
+
+        // Representa o jogador
+        public class Player
+        {
+            public Hand PlayerHand { get; set; } = new Hand();
+            public decimal Balance { get; set; }
+
+            public void PlaceBet(decimal amount)
+            {
+                Balance -= amount;
+            }
+
+            public void Hit(Card card)
+            {
+                PlayerHand.Cards.Add(card);
+            }
+
+            public bool CanDouble() => PlayerHand.Cards.Count == 2;
+
+            // Dobrar aposta
+            public void DoubleBet(decimal originalBet)
+            {
+                Balance -= originalBet; // Dobrar a aposta original
+            }
+        }
+        public class ScoreManager
+        {
+            public int CalculateHandValue(List<Card> hand)
+            {
+                int totalValue = 0;
+                int aceCount = 0;
+
+                foreach (var card in hand)
+                {
+                    if (int.TryParse(card.Value, out int cardValue))
+                    {
+                        // Cartas numéricas (2-10)
+                        totalValue += cardValue;
+                    }
+                    else if (card.Value == "JACK" || card.Value == "QUEEN" || card.Value == "KING")
+                    {
+                        // Cartas de face valem 10
+                        totalValue += 10;
+                    }
+                    else if (card.Value == "ACE")
+                    {
+                        // Ás pode valer 1 ou 11
+                        aceCount++;
+                        totalValue += 11; // Inicialmente, considere o Ás como 11
+                    }
+
+                }
+
+                // Ajusta os Áses para 1 se a pontuação exceder 21
+                while (totalValue > 21 && aceCount > 0)
+                {
+                    totalValue -= 10; // Considera o Ás como 1
+                    aceCount--;
+                }
+
+                return totalValue;
+            }
+        }
+
+    }
+}
